1854e24ab01aceed2c870f9e72621eeb19302fd7
shally3009 <mittalshally30@gmail.com>
2025-02-13 16:11:02 +0530

    done

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-const {connect} = require('mongoose');
+const { connect } = require('mongoose');
 
 const connectDB = async (url) => {
     try {
-        await connect(url)
+        console.log('Attempting to connect to the database...');
+        await connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to the database');
     } catch (error) {
         console.error('Error connecting to the database');
         console.error(error);
     }
-}
-module.exports = {connectDB};
+};
+
+module.exports = { connectDB };
