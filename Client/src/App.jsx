b63e456e1fc939200ade88753916671f108b521c
shally3009 <mittalshally30@gmail.com>
2025-02-17 15:34:13 +0530

    done

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Frontpage from './components/Frontpage';
+
+function App() {
+  return (
+    <div>
+      <Frontpage />
+    </div>
+  );
+}
+
+export default App;
